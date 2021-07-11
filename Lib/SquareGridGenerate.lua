bd8f339fbf3d6b3e77e7586f9e9227236e4e5193
Sam Trenholme <git@samiam.org>
2021-07-11 04:15:02 -0700

    SquareGridGenerate.lua: Add semisphere support
    This is a map which is topologically equivalent (homeomorphic) to a
    sphere which is fairly easy to implement on a square grid.  It allows
    one to have a reasonable planet simulation without the headaches of trying
    to tile a sphere.
    
    The way it works is, if we go “above” the top of the map, we end up on
    the top of the map, but halfway over.  Ditto with the bottom of the map.
    
    This way, each local neighborhood is the same (all tiles are squares;
    all squares allow movement in eight directions), but the map works like
    a sphere (we can more quickly move from one point to another east or west
    near the north and south poles).
    
    One issue with semispheres is that, if this form is used in a game,
    once we cross the north or south pole, the map becomes a vertically
    flipped mirror image.  One can either play in the “flipped” form, or
    we have the user interface “correct” their orientation after they
    cross the pole.

--- Lib/SquareGridGenerate.lua
+++ Lib/SquareGridGenerate.lua
@@ -7,6 +7,29 @@ function createGridMap(sizeX, sizeY, wrap)
     return "x" .. tonumber(X) .. "y" .. tonumber(Y)
   end
 
+  -- A semisphere is a special form which is topologically equivalent to
+  -- a sphere.  Wraps on the X axis go from far left to far right, and from
+  -- far right to far left.  Wraps on the Y axis go from the top square on 
+  -- the Y axis to the top square halfway over; ditto with the bottom square
+  -- This allows a reasonable simulation of a sphere on a square grid
+  -- without making the poles impassible
+  -- Note: If this were played in a game, the world would become a vertically
+  -- flipped mirror image after going below the south pole or above the 
+  -- north pole.
+  function handleSemisphere(X, Y, sizeX, sizeY)
+    if Y < 0 then 
+      Y = 0
+      X = X + math.floor(sizeX / 2)
+    end
+    if Y >= sizeY then
+      Y = sizeY
+      X = X + math.floor(sizeX / 2)
+    end
+    if X < 0 then X = X + sizeX end
+    if X >= sizeX then X = X - sizeX end
+    return X, Y
+  end
+
   -- When deciding an adjacent square, handle underflow and overflow
   function handleWrap(V, sizeV, goodWrap, wrap)
     if V < 0 then
@@ -33,7 +56,8 @@ function createGridMap(sizeX, sizeY, wrap)
   if wrap ~= "NONE" and 
      wrap ~= "TORIC" and 
      wrap ~= "WRAPX" and 
-     wrap ~= "WRAPY" then
+     wrap ~= "WRAPY" and
+     wrap ~= "SEMISPHERE" then
     return {ERROR = "Invalid wrap value"}
   end
   sizeX = math.floor(tonumber(sizeX))
@@ -41,16 +65,22 @@ function createGridMap(sizeX, sizeY, wrap)
   if sizeX < 8 then sizeX = 8 end
   if sizeY < 8 then sizeY = 8 end
 
-  local newMap = {ROOT = "x0y0"}
+  -- As a convention, lower case/numeric is node name, UPPER CASE is meta
+  -- information about the map
+  local newMap = {ROOT = "x0y0", WRAP = wrap}
 
   for X = 0, (sizeX - 1) do
     for Y = 0, (sizeY - 1) do
-      local node = {name = nodeName(X,Y)}
+      local node = {name = nodeName(X,Y), X = X, Y = Y}
       node.adjacent = {}
       for dirX = -1, 1 do
         for dirY = -1, 1 do
-	  placeX = handleWrap(X + dirX, sizeX, "WRAPX", wrap)
-	  placeY = handleWrap(Y + dirY, sizeY, "WRAPY", wrap)
+	  if wrap ~= "SEMISPHERE" then
+	    placeX = handleWrap(X + dirX, sizeX, "WRAPX", wrap)
+	    placeY = handleWrap(Y + dirY, sizeY, "WRAPY", wrap)
+	  else
+	    placeX, placeY = handleSemisphere(X+dirX, Y+dirY, sizeX, sizeY)
+	  end
 	  if placeX and placeY and (dirX ~= 0 or dirY ~= 0) then
 	    node.adjacent[#node.adjacent + 1] = nodeName(placeX,placeY)
 	  end
