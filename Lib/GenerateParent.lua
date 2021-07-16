4ddde90f855bf018c0a8ce6a789a3833ef45955f
Sam Trenholme <git@samiam.org>
2021-07-16 02:28:36 -0700

    Begin work on "brute force" distance calculator

--- Lib/GenerateParent.lua
+++ Lib/GenerateParent.lua
@@ -4,10 +4,22 @@ MapMeta = {}
 MapMeta.__index = GridMeta
 
 function MapMeta:new()
-  return setmetatable({distanceMap = nil, ROOT = nil}, self)
+  return setmetatable({DistanceMap = nil, ROOT = nil}, self)
 end
 
 function MapMeta:distance()
   -- CODE HERE: Default high memory usage "brute force" distance calculator
+
+  -- This function only fills in the distances one away
+  function distanceMapOneNode(node, view, distance) 
+    if not self[view] then return false end
+    if not self[view]["adjacent"] then return false end    
+    for a = 1,#self[view]["adjacent"] do
+      if not self.DistanceMap[node][#self[view]["adjacent"][1]] then
+        self.DistanceMap[node][#self[view]["adjacent"][1]] = 
+            distance + self.DistanceMap[node][#self[view]["adjacent"][2]]
+      end
+    end
+  end -- distanceMapOneNode()
 
 end
