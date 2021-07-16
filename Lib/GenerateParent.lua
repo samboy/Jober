9c454f99a7a5b3e2f2b3fdd6af35270b39b88d23
Sam Trenholme <git@samiam.org>
2021-07-16 02:47:00 -0700

    Some more work on hard brute force distance calculator
    Here's the problem: If we use a breadth-first distance map builder,
    we can usually get a fairly short distance from point A to point B on
    the map.
    
    However, with a square grid and 1.4 distances over diagonals, we can
    have issues like this:
    
    A B C
    D E F
    G H I
    
    To go from H to B, the fastest path is H -> E -> B.  With a breadth
    first distance finder, it will find H -> D (1.4).  Once at H->D, it will
    add H->D->B (2.8, instead of 2 for H->E->B).
    
    A reasonable solution would be to do the breath first search staring
    with closer nodes (so we process H->E, H->G, and H->I before H->D and
    H->F).

--- Lib/GenerateParent.lua
+++ Lib/GenerateParent.lua
@@ -10,16 +10,21 @@ end
 function MapMeta:distance()
   -- CODE HERE: Default high memory usage "brute force" distance calculator
 
-  -- This function only fills in the distances one away
-  function distanceMapOneNode(node, view, distance) 
+  -- This function only fills in the distances one away, but it returns
+  -- a list of nodes to fill out next
+  function distanceMapOneAway(node, view, distance) 
+    local out = {}
     if not self[view] then return false end
     if not self[view]["adjacent"] then return false end    
     for a = 1,#self[view]["adjacent"] do
-      if not self.DistanceMap[node][#self[view]["adjacent"][1]] then
-        self.DistanceMap[node][#self[view]["adjacent"][1]] = 
-            distance + self.DistanceMap[node][#self[view]["adjacent"][2]]
+      local neighbor = self[view]["adjacent"][a][1]
+      if not self.DistanceMap[node][neighbor] then
+        self.DistanceMap[node][neighbor] = 
+            distance + self.DistanceMap[node][self[view]["adjacent"][a][2]]
+	out[#out + 1] = neighbor
       end
     end
+    return out
   end -- distanceMapOneNode()
 
 end
