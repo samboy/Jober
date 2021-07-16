c51f6281e67a59118ebc79481122d73be6ffd420
Sam Trenholme <git@samiam.org>
2021-07-16 03:38:36 -0700

    We now avoid longer "zig zag" paths
    In order to make our pathfinder not use exponential time, we will
    assume that, for a given path, the shortest path between any two
    nodes (A and Z) X spaces apart with a A->B->C->...->Z path will have
    A->B be the shortest one-step path between A and any node which takes
    us to Z in X steps.
    
    This works for the usual square, isomorphic square, and hex grids, but
    can fail with some "adventure map" grids.  Let's take this:
    
    	A -- 2 --> B -- 1 -\
    	|                   +--> D
    	+ -- 1 --> C -- 9 -/
    
    Here, A->B->D is three points total, so is the fastest path from A to D.
    That's two points to go from A to B and 1 point to go from B to D.
    
    A->C->D is longer: 10 points.  One point to go from A to C and nine points
    to go from C to D.  However, our pathfinder will choose A->C->D
    because it has the fastest path from A to an adjacent node which takes us
    to D in two steps.
    
    So, it's not a perfect pathfinder for any given grid, but it works for
    for "normal" wargame grids used in the context of a procedural/random
    map generator.

--- Lib/GenerateParent.lua
+++ Lib/GenerateParent.lua
@@ -14,16 +14,26 @@ function MapMeta:distance()
   -- a list of nodes to fill out next
   function distanceMapOneAway(node, view, distance) 
     local out = {}
+    local dmap = {}
     if not self[view] then return false end
     if not self[view]["adjacent"] then return false end    
     for a = 1,#self[view]["adjacent"] do
       local neighbor = self[view]["adjacent"][a][1]
+
+      -- Note the distance from one node to another
+      dmap[neighbor] = self[view]["adjacent"][a][2]
+
       if not self.DistanceMap[node][neighbor] then
         self.DistanceMap[node][neighbor] = 
             distance + self.DistanceMap[node][self[view]["adjacent"][a][2]]
 	out[#out + 1] = neighbor
       end
     end
+
+    -- To avoid longer "zig zag" paths, we want to have the shortest paths
+    -- first in the output table
+    table.sort(out, function(a, b) return dmap[a] < dmap[b] end)
+
     return out
   end -- distanceMapOneNode()
 
